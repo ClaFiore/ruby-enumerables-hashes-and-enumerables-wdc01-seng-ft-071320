9e44753f05504f4f0c0b88db073684e48b5a2976
Claudia Borghini <claborghini@gmail.com>
2020-07-06 13:21:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,11 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each {|suite, name|
-if suite = :suite_a && name.start_with? ("A")
+  passengers.each do |suite, name|
+if suite == :suite_a && name.start_with? ("A")
   winner = name
 end
-  }
+end
   winner
   # add the code snippet here!
 end
